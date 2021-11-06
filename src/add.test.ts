6f93fcaab91b307b48e05dcba28f9c557bf122f2
oskarlh <g@oskar-lh.name>
2021-11-06 22:37:11 +0100

    Make a test fail to test the GitHub Action

--- src/add.test.ts
+++ src/add.test.ts
@@ -2,7 +2,7 @@ import add from "./add";
 
 describe("add", () => {
 	it("adds two numbers", () => {
-		expect(add(1, 1)).toBe(2);
+		expect(add(1, 2)).toBe(2);
 		expect(add(8, -4)).toBe(4);
 	});
 });
