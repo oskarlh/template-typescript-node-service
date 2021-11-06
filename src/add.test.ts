967c7b763d2cf5b9a070f09f87e0d75896d48c60
oskarlh <g@oskar-lh.name>
2021-11-06 04:56:57 +0100

    Use the Jest VSCode extension

--- src/add.test.ts
+++ src/add.test.ts
@@ -1,7 +1,8 @@
 import add from "./add";
 
 describe("add", () => {
-	it("adds 1 and 1", () => {
+	it("adds two numbers", () => {
 		expect(add(1, 1)).toBe(2);
+		expect(add(8, -4)).toBe(4);
 	});
 });
