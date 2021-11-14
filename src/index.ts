a59178085074bd336e647d9ba26736d5e6a70fc4
oskarlh <g@oskar-lh.name>
2021-11-14 04:51:34 +0100

    Use top-level await

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,37 @@ import main from "./main.js";
 const SHUTDOWN_LINGERING_REQUESTS_TIMEOUT_SECONDS = 2 * 60;
 const SHUTDOWN_MAIN_TIMEOUT_SECONDS = 15 * 60;
 
-(async () => {
-	const sigintController = new AbortController();
+const sigintController = new AbortController();
 
-	let mainFinished = false;
+let mainFinished = false;
 
-	// Handle kill signals
-	process.once("SIGINT", () => {
-		sigintController.abort();
-		setTimeout(() => {
-			if (!mainFinished) {
-				console.error(
-					"Process killed after waiting for main() to finish for %fs",
-					SHUTDOWN_MAIN_TIMEOUT_SECONDS,
-				);
-				process.kill(process.pid, "SIGINT");
-			}
-		}, SHUTDOWN_MAIN_TIMEOUT_SECONDS * 1000).unref();
-	});
-
-	try {
-		await main(sigintController.signal);
-	} catch (error) {
-		console.error("main() threw:\n%o", error);
-	}
-	mainFinished = true;
-
-	// This SHOULD NOT be necessary, but from experience it is not uncommon
-	// for developers to forget to close sockets, timers etc.
+// Handle kill signals
+process.once("SIGINT", () => {
+	sigintController.abort();
 	setTimeout(() => {
-		console.error(
-			"Failed to shut down gracefully after waiting %fs for requests to terminate",
-			SHUTDOWN_LINGERING_REQUESTS_TIMEOUT_SECONDS,
-		);
-		process.kill(process.pid, "SIGINT");
-	}, SHUTDOWN_LINGERING_REQUESTS_TIMEOUT_SECONDS * 1000).unref();
-})();
+		if (!mainFinished) {
+			console.error(
+				"Process killed after waiting for main() to finish for %fs",
+				SHUTDOWN_MAIN_TIMEOUT_SECONDS,
+			);
+			process.kill(process.pid, "SIGINT");
+		}
+	}, SHUTDOWN_MAIN_TIMEOUT_SECONDS * 1000).unref();
+});
+
+try {
+	await main(sigintController.signal);
+} catch (error) {
+	console.error("main() threw:\n%o", error);
+}
+mainFinished = true;
+
+// This SHOULD NOT be necessary, but from experience it is not uncommon
+// for developers to forget to close sockets, timers etc.
+setTimeout(() => {
+	console.error(
+		"Failed to shut down gracefully after waiting %fs for requests to terminate",
+		SHUTDOWN_LINGERING_REQUESTS_TIMEOUT_SECONDS,
+	);
+	process.kill(process.pid, "SIGINT");
+}, SHUTDOWN_LINGERING_REQUESTS_TIMEOUT_SECONDS * 1000).unref();
