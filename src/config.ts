957510fdf68a2fee77a4c243692039b4655b71ee
Oskar Larsson Högfeldt <oskar@oskar.pm>
2023-04-22 02:47:51 +0200

    Add dotenv configuration

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import { existsSync } from "fs";
+
+function loadEnvFiles() {
+	if (existsSync(".env.local")) {
+		config({ path: ".env.local" });
+	}
+	config({ path: ".env" });
+}
+loadEnvFiles();
+
+export const SECONDS_BETWEEN_MESSAGES =
+	Number(process.env.SECONDS_BETWEEN_MESSAGES) || 5;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
+import { SECONDS_BETWEEN_MESSAGES } from "./config.js";
 import isAbortError from "./isAbortError.js";
 import sleep from "./sleep.js";
 
 export default async function main(shutdownSignal: AbortSignal): Promise<void> {
 	try {
 		console.log("Hello!");
-		await sleep(5, shutdownSignal);
-		console.log("5 seconds have passed");
-		await sleep(5, shutdownSignal);
-		console.log("Another 5 seconds have passed");
+		await sleep(SECONDS_BETWEEN_MESSAGES, shutdownSignal);
+		console.log(`${SECONDS_BETWEEN_MESSAGES} seconds have passed`);
+		await sleep(SECONDS_BETWEEN_MESSAGES, shutdownSignal);
+		console.log(`Another ${SECONDS_BETWEEN_MESSAGES} seconds have passed`);
 	} catch (error) {
 		if (isAbortError(error)) {
 			console.log("Aborted.");
