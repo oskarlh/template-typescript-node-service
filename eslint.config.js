48f765bd4dd80b45a86349ca9e076ec11112a967
Oskar Larsson Högfeldt <oskar@oskar.pm>
2024-07-05 01:46:38 +0200

    Lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default tseslint.config(
 				},
 			],
 			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/restrict-template-expressions": ["off"],
 			curly: ["warn", "multi-line"],
 			"no-constant-condition": ["error", { checkLoops: false }],
 			"no-else-return": ["warn", { allowElseIf: true }],
--- suppressExperimentalWarnings.cjs
+++ suppressExperimentalWarnings.cjs
@@ -1,11 +1,15 @@
 "use strict";
+/* eslint-disable */
 
 // This is a bit of a hack. It suppresses Node's warnings about experimental features.
 // Use it like this: node --require=./NAME-OF-THIS-FILE-HERE example.js
 
 const { emitWarning } = process;
 process.emitWarning = (warning, ...args) => {
-	if (args[0] !== "ExperimentalWarning" && (typeof args[0] !== "object" || args[0].type !== "ExperimentalWarning")) {
+	if (
+		args[0] !== "ExperimentalWarning" &&
+		(typeof args[0] !== "object" || args[0].type !== "ExperimentalWarning")
+	) {
 		return emitWarning(warning, ...args);
 	}
 };
